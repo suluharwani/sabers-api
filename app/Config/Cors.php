28b59a4ff04e2b31544d7eb2f12b2bc443bff993
Suluh Arwani <suluharwani007@gmail.com>
2025-05-29 09:32:36 +0700

    Update Cors.php

--- app/Config/Cors.php
+++ app/Config/Cors.php
@@ -34,7 +34,7 @@ class Cors extends BaseConfig
          *   - ['http://localhost:8080']
          *   - ['https://www.example.com']
          */
-        'allowedOrigins' => ['*'],
+        'allowedOrigins' => ['https://sabers.web.id'],
 
         /**
          * Origin regex patterns for the `Access-Control-Allow-Origin` header.
